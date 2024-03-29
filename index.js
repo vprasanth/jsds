f1d4b9a8d584b2233cd00b0c06070a59fb1e5ecb
Prasanth Vaaheeswaran <prasanthv@icloud.com>
2024-03-29 16:49:04 -0400

    test: write tests for minheap

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-import { MinHeap, HeapItem } from "./lib/min-heap.js";
 import { RateLimit, RateLimiter } from "./utils/rate-limiter.js";
 import { execSync } from "child_process";
-
-const h = new MinHeap();
-
-const arr = [4, 6, 34, 12, 1, 5];
-
-// console.log(h);
-
-for (let item of arr) {
-  h.push(new HeapItem(item));
-}
-
-// console.log(h);
-// console.log(h.pop());
 
 const testRl = new RateLimit("test", 60, 5);
 const rl = new RateLimiter([testRl]);
